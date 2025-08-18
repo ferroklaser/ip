f698fbdd76cbc93e439f012b9a49870cc5b21e47
Win Paing Oo <winpaing000@gmail.com>
2025-08-18 21:36:15 +0800

    Add task marking

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -4,23 +4,32 @@ import java.util.ArrayList;
 public class Echo {
     public static void main(String[] args) {
         String logo = "Echo";
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
         System.out.println("Hello! I'm " + logo);
         Scanner scanner = new Scanner(System.in);
         System.out.println("What can I do for you?");
 
         while (true) {
             String command = scanner.nextLine();
+            String[] words = command.split(" ");
             if (command.equals("bye")) {
                 break;
             } else if (command.equals("list")) {
                 int index = 1;
-                for (String task : list) {
-                    System.out.println(index + ". " + task);
+                System.out.println("Let's take a look at tasks in your list:");
+                for (Task task : list) {
+                    System.out.println(index + "." + task);
                     index++;
                 }
+            } else if (words[0].equals("mark")){
+                int index = Integer.parseInt(words[1]);
+                Task task = list.get(index - 1);
+                task.markAsDone();
+                System.out.println("Good job! I've marked this task as done:");
+                System.out.println("  " + task);
             } else {
-                list.add(command);
+                Task t = new Task(command);
+                list.add(t);
                 System.out.println("added: " + command);
             }
         }
