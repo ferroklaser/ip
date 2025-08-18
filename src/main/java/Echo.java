8dabd0f58c4ad485b499c3c294c26ea2825a5d27
Win Paing Oo <winpaing000@gmail.com>
2025-08-18 20:53:01 +0800

    Add echo and bye command

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class Echo {
     public static void main(String[] args) {
         String logo = "Echo";
         System.out.println("Hello! I'm " + logo);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("What can I do for you?");
+
+        while (true) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                break;
+            } else {
+                System.out.println(command);
+            }
+        }
         System.out.println("Byeeee, cya!");
     }
 }
