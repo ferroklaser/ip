79b9b83d5c06ad5bff639d7317bcb6cf26515467
Win Paing Oo <winpaing000@gmail.com>
2025-08-18 20:20:56 +0800

    Add greeting

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -1,6 +1,7 @@
 public class Echo {
     public static void main(String[] args) {
         String logo = "Echo";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm " + logo);
+        System.out.println("What can I do for you?");
     }
 }
