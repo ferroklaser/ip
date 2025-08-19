78b326efdc45789f2aa98a2f0be629f9af6f4ba2
Win Paing Oo <winpaing000@gmail.com>
2025-08-19 18:28:56 +0800

    Add Todo task adding

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -1,17 +1,19 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Echo {
     public static void main(String[] args) {
         String logo = "Echo";
         ArrayList<Task> list = new ArrayList<>();
+        List<String> taskTypes = List.of("todo", "event", "deadline");
         System.out.println("Hello! I'm " + logo);
         Scanner scanner = new Scanner(System.in);
         System.out.println("What can I do for you?");
 
         while (true) {
             String command = scanner.nextLine();
-            String[] words = command.split(" ");
+            String[] parts = command.split(" ", 2);
             if (command.equals("bye")) {
                 break;
             } else if (command.equals("list")) {
@@ -21,14 +23,26 @@ public class Echo {
                     System.out.println(index + "." + task);
                     index++;
                 }
-            } else if (words[0].equals("mark")) {
-                int index = Integer.parseInt(words[1]);
+            } else if (taskTypes.contains(parts[0])) {
+                Task t = null;
+                if (parts[0].equals("todo")) {
+                    t = new Todo(parts[1]);
+                    list.add(t);
+                }
+//                else if (parts[0].equals("deadline")){
+//                    String[] taskParts = parts[1].split(" /by ")
+//                }
+                System.out.println("Ok, I've added this task for you");
+                System.out.println("  " + t);
+                System.out.println("Now you have " + list.size() + " tasks in the list");
+            } else if (parts[0].equals("mark")) {
+                int index = Integer.parseInt(parts[1]);
                 Task task = list.get(index - 1);
                 task.markAsDone();
                 System.out.println("Good job! I've marked this task as done:");
                 System.out.println("  " + task);
-            } else if (words[0].equals("unmark")) {
-                int index = Integer.parseInt(words[1]);
+            } else if (parts[0].equals("unmark")) {
+                int index = Integer.parseInt(parts[1]);
                 Task task = list.get(index - 1);
                 task.markAsUndone();
                 System.out.println("Alright, I've marked this task as not done for you:");
