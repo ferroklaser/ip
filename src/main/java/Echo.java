6da022e15ead2eb0b22f867823d5344a1e986687
Win Paing Oo <winpaing000@gmail.com>
2025-08-27 23:49:59 +0800

    Add Parser Class

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -1,8 +1,5 @@
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.List;
 
 enum Command {
     TODO,
@@ -18,26 +15,16 @@ enum Command {
 
 public class Echo {
     public static void main(String[] args) {
-//        String logo = "Echo";
         Storage storage = new Storage("data/echo.txt");
-//        List<Task> list = storage.readFile();
         UI ui = new UI();
         TaskList list = new TaskList(storage.readFile());
-//        System.out.println("Hello! I'm " + logo);
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("What can I do for you?");
         ui.showWelcome();
 
         while (true) {
             try {
                 String input = ui.readCommand();
-                String[] parts = input.split(" ", 2);
-                Command command;
-               try {
-                   command = Command.valueOf(parts[0].toUpperCase());
-               } catch (IllegalArgumentException e) {
-                   command = Command.DEFAULT;
-               }
+                String[] parsedInput = Parser.parse(input);
+                Command command = Command.valueOf(parsedInput[0]);
                 switch (command) {
                 case BYE:
                     ui.showExit();
@@ -51,34 +38,22 @@ public class Echo {
                 case DEADLINE:
                 case EVENT:
                     Task t = null;
-                    if (parts.length < 2 || parts[1].isEmpty()) {
-                        throw new EchoException("Wait a min! Your description cannot be empty");
-                    }
                     switch (command) {
                         case TODO:
-                            t = new Todo(parts[1]);
+                            t = new Todo(parsedInput[1]);
                             list.addTask(t);
                             break;
                         case DEADLINE:
-                            String[] deadlineParts = parts[1].split(" /by ");
-                            if (deadlineParts.length < 2 || deadlineParts[1].isEmpty()) {
-                                throw new EchoException("Wait a min! Your deadline cannot be empty");
-                            }
                             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
-                            LocalDateTime deadline = LocalDateTime.parse(deadlineParts[1], formatter);
-                            t = new Deadline(deadlineParts[0], deadline);
+                            LocalDateTime deadline = LocalDateTime.parse(parsedInput[2], formatter);
+                            t = new Deadline(parsedInput[1], deadline);
                             list.addTask(t);
                             break;
                         case EVENT:
-                            String[] eventParts = parts[1].split(" /from ");
-                            String[] fromToParts = eventParts[1].split(" /to ");
-                            if (fromToParts[0].isEmpty() || fromToParts[1].isEmpty()) {
-                                throw new EchoException("Wait a min! Your dates cannot be empty");
-                            }
                             DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
-                            LocalDateTime start = LocalDateTime.parse(fromToParts[0], formatter1);
-                            LocalDateTime end = LocalDateTime.parse(fromToParts[1], formatter1);
-                            t = new Event(eventParts[0], start, end);
+                            LocalDateTime start = LocalDateTime.parse(parsedInput[2], formatter1);
+                            LocalDateTime end = LocalDateTime.parse(parsedInput[3], formatter1);
+                            t = new Event(parsedInput[1], start, end);
                             list.addTask(t);
                             break;
                     }
@@ -86,21 +61,21 @@ public class Echo {
                     ui.showListSize(list);
                     break;
                 case MARK: {
-                    int index = Integer.parseInt(parts[1]);
+                    int index = Integer.parseInt(parsedInput[1]);
                     Task task = list.getTask(index);
                     task.markAsDone();
                     ui.showMarkedTask(task);
                     break;
                 }
                 case UNMARK: {
-                    int index = Integer.parseInt(parts[1]);
+                    int index = Integer.parseInt(parsedInput[1]);
                     Task task = list.getTask(index);
                     task.markAsUndone();
                     ui.showUnmarkedTask(task);
                     break;
                 }
                 case DELETE: {
-                    int index = Integer.parseInt(parts[1]);
+                    int index = Integer.parseInt(parsedInput[1]);
                     Task task = list.deleteTask(index);
                     ui.showDeletedTask(task);
                     ui.showListSize(list);
